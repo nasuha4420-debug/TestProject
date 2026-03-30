1349bbac0915e74c8d417b8d9ede5cf0343ba9e4
nasuh <nasuh@LAPTOP-7J0KDG1N>
2026-03-31 02:24:32 +0800

    lab csc

--- demoJava/src/demoJava/MakingMoneyATM.java
+++ demoJava/src/demoJava/MakingMoneyATM.java
@@ -4,7 +4,10 @@ public class MakingMoneyATM {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+      System.out.println("************************************");
+      System.out.println("* Making Money Bank ATM Service    *");
+      System.out.println("************************************");
+      System.out.println("\nWelcome!");
 	}
 
 }
--- demoJava/src/demoJava/Student.java
+++ demoJava/src/demoJava/Student.java
@@ -4,7 +4,9 @@ public class Student {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+     System.out.println("Nama:Nasuha");
+     System.out.println("Matrik:229031");
+     System.out.println("Alamat:No 48,Pantai Remis,Perak");
 	}
 
 }
